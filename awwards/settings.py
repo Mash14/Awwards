30d58fb7669f730100bf78475de9c8b8307f9b0a
mash <mashalonzo741@gmail.com>
2021-10-22 14:24:39 +0300

    Add bootstrap

--- awwards/settings.py
+++ awwards/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects.ProjectsConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
