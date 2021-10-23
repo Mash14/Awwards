3784ca5fd33c6628adfac68187317015db34d8c2
mash <mashalonzo741@gmail.com>
2021-10-23 08:22:50 +0300

    Add tinymce to Project model

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
 
 class Project(models.Model):
     title = models.CharField(max_length=70)
     image = models.ImageField(upload_to = 'l_page/')
-    description = models.CharField(max_length=500)
+    description = HTMLField()
     link = models.CharField(max_length=120)
-    
