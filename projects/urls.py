23f351c20913dd6dc50cec4361d5619ad801e928
mash <mashalonzo741@gmail.com>
2021-10-24 19:45:09 +0300

    Add the route to home age

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
+from django.conf.urls import url
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-
+    url('^$',views.home_page,name = 'homePage'),
 ]
 
 if settings.DEBUG:
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Profile,Project
 
 # Create your views here.
+
+def home_page(request):
+    profiles = Profile.objects.all()
+    projects = Project.objects.all()
+
+    return render(request, 'index.html', {'profiles':profiles,'projects':projects})
