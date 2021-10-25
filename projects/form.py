48ea6fc887ea2c4d8f94e4f5e387c8eda5b5d112
mash <mashalonzo741@gmail.com>
2021-10-25 03:22:48 +0300

    Add the project form

---
+++ projects/form.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Profile,Project
+from django.contrib.auth.models import User
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['owner','profile','pub_date']
